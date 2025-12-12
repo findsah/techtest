a6f0089a1e4aeeaaf00cdac7eb81e2cdeecb4bd1
syedalihassan03@gmail.com <syedalihassan03@gmail.com>
2025-12-12 22:14:40 +0000

    api-integration

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 
+// Simple CORS headers for local dev
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // Sample games/products data
 const games = [
   {
